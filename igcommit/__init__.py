cc318e1a08ba10fad3f84b000f7fc8e1b7977f92
Emre Hasegeli <emre@hasegeli.com>
2017-02-24 15:56:22 +0100

    Improve setup.py

---
+++ igcommit/__init__.py
@@ -0,0 +1,6 @@
+"""igcommit - The main module
+
+Copyright (c) 2017, InnoGames GmbH
+"""
+
+VERSION = (1, 0)
--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
 #!/usr/bin/env python
 """igcommit - Setup
 
-Copyright (c) 2016, InnoGames GmbH
+Copyright (c) 2017, InnoGames GmbH
 """
 
 from setuptools import setup
 
+from igcommit import VERSION
+from igcommit.config import checks
+
+
+with open('README.rst') as fd:
+    readme = fd.read()
+
 setup(
     name='igcommit',
-    version='1.0',
+    version='.'.join(str(v) for v in VERSION),
     url='https://github.com/innogames/igcommit',
     packages=['igcommit'],
+    author='InnoGames System Administration',
+    author_email='it@innogames.com',
+    license='MIT',
+    platforms='POSIX',
+    description='Git pre-receive hook to check commits and code style',
+    long_description=readme,
+    keywords=(
+        'syntax-checker git git-hook python ' +
+        ' '.join(c.args[0] for c in checks if hasattr(c, 'args'))
+
+    ),
     entry_points={
         'console_scripts': [
             'igcommit-receive=igcommit.prereceive:main',
