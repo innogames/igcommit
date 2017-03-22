6a4caa06d7755825568b73a886a872e43ae912fd
Emre Hasegeli <emre@hasegeli.com>
2017-03-22 10:02:02 +0100

    Flush stdout before printing traceback
    Zheng Wei reported on #8 the traceback of the encoding error appearing
    in between the problems.  The problems are printed to stdout.  Flushing
    stdout before printing the traceback would hopefully prevent this
    issue.

--- igcommit/prereceive.py
+++ igcommit/prereceive.py
@@ -5,6 +5,7 @@ Copyright (c) 2016, InnoGames GmbH
 
 from collections import defaultdict
 from fileinput import input
+from sys import stdout
 from traceback import print_exc
 
 from igcommit.base_check import CheckState, prepare_checks
@@ -96,6 +97,9 @@ def main():
     try:
         state = Runner().run()
     except:
+        # Flush the problems we have printed so far to avoid the traceback
+        # appearing in between them.
+        stdout.flush()
         print_exc()
     else:
         if state >= CheckState.FAILED:
